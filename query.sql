e2dde5f34be26965d46306119097ef6cf493d72d
g-adamante <gabriel.henrique.a@gmail.com>
2017-10-04 21:27:01 -0300

    Update query.sql
    Alterando o Banco de dados pois usamos enum para lidar com as categorias

--- query.sql
+++ query.sql
@@ -8,12 +8,6 @@ user_type  number(8) NOT NULL,
 PRIMARY KEY (user_id)
 );
 
-CREATE TABLE categories (
-cat_id          number(8) NOT NULL,
-cat_name        VARCHAR(255) NOT NULL,
-cat_description     VARCHAR(255) NOT NULL,
-PRIMARY KEY (cat_id)
-);
 
 CREATE TABLE topics (
 topic_id        number(8) NOT NULL,
@@ -22,7 +16,6 @@ topic_date      TIMESTAMP NOT NULL,
 topic_cat       number(8) NOT NULL,
 topic_by        number(8) NOT NULL,
 PRIMARY KEY (topic_id),
-FOREIGN KEY(topic_cat) REFERENCES categories(cat_id),
 FOREIGN KEY(topic_by) REFERENCES users(user_id)
 );
 
